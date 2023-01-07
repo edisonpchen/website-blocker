3f91c784d8809f84173cd63d7042ee335cf81a66
Edison Chen <30671669+edisonpchen@users.noreply.github.com>
2023-01-06 21:54:22 -0800

    Add files via upload

---
+++ website-blocker.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+end_time = datetime(2021, 1, 1, 20)
+
+sites_to_block = ['www.facebook.com', 'facebook.com']
+
+hosts_path = "/etc/hosts"
+
+redirect = "127.0.0.1"
+
